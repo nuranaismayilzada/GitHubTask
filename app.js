996d4807bcf2a511b75f50750614550525464423
nuranaismayilzada <bd6nyw4qq@code.edu.az>
2024-04-23 19:08:18 +0400

    finished

--- app.js
+++ app.js
@@ -22,54 +22,47 @@
 // let bio = document.querySelector(".bio")
 // let repos = document.querySelector(".repos")
 // let locationn = document.querySelector(".location")
-// let followers=document.querySelector(".followers")
-// let following=document.querySelector(".following")
-// let joined=document.querySelector(".joined")
-// let blog=document.querySelector(".blog")
-// let twitterUsername=document.querySelector(".twitterUsername")
-// let email=document.querySelector(".email")
-// let agithub=document.querySelector(".agithub")
+// let followers = document.querySelector(".followers")
+// let following = document.querySelector(".following")
+// let joined = document.querySelector(".joined")
+// let blog = document.querySelector(".blog")
+// let twitterUsername = document.querySelector(".twitterUsername")
+// let email = document.querySelector(".email")
+// let agithub = document.querySelector(".agithub")
 // searchBtn.addEventListener("click", (e) => {
 //     e.preventDefault()
-//     fetch("https://api.github.com/users").then((res) =>
+//     fetch(`https://api.github.com/users/${searchInput.value}`).then((res) =>
 //         res.json()).then((data) => {
-//            data.forEach(element => {
-//                 if (searchInput.value==element.login) {
-//                     avatar.src=element.avatar_url
-//                     name.innerText=element.login
-//                     let url=`https://api.github.com/users/${name.value}`
-//                     fetch(url).then((res)=>res.json()).then((dat)=>{
-//                         method:"GET";
-//                         console.log(dat);
-//                         bio.innerText=dat.bio
-//                         if (dat.location==null) {
-//                             locationn.innerText="not mentioned"
-//                         }
-//                         else{
-//                             locationn.innerText=dat.location
-//                         }
-//                         followers.innerText=dat.followers
-//                         following.innerText=dat.following
-//                         repos.innerText=dat.public_repos
-//                         blog.innerText=dat.blog
-//                         if (dat.twitter_username==null) {
-//                             twitterUsername.innerText='dont use'
-//                         }else{
+//                 if (searchInput.value == data.login) {
+//                     avatar.src = data.avatar_url
+//                     name.innerText = data.login
+//                     bio.innerText = data.bio
+//                     if (data.location == null) {
+//                         locationn.innerText = "not mentioned"
+//                     }
+//                     else {
+//                         locationn.innerText = data.location
+//                     }
+//                     followers.innerText = data.followers
+//                     following.innerText = data.following
+//                     repos.innerText = data.public_repos
+//                     blog.innerText = data.blog
+//                     if (data.twitter_username == null) {
+//                         twitterUsername.innerText = 'dont use'
+//                     } else {
 
-//                             twitterUsername.innerText=dat.twitter_username
-//                         }
-//                         joined.innerText="Joined: " + dat.created_at
-//                         if (dat.email==null) {
-//                             email.innerText="Email: dont use"
-//                         }
-//                         else{
-//                            email.innerText="Email:" + dat.email
-//                         }
-//                     })
+//                         twitterUsername.innerText = data.twitter_username
+//                     }
+//                     joined.innerText = "Joined: " + data.created_at
+//                     if (data.email == null) {
+//                         email.innerText = "Email: dont use"
+//                     }
+//                     else {
+//                         email.innerText = "Email:" + data.email
+//                     }
 //                 }
-//            });
+//             });
 //         })
-// })
 
 
 
@@ -151,5 +144,6 @@ searchBtn.addEventListener("click", (e) => {
             twitterUsername.innerText = data.twitter_username ? data.twitter_username : "Not provided"
             joined.innerText = "Joined: " + new Date(data.created_at).toLocaleDateString()
             email.innerText = data.email ? "Email: " + data.email : "Email: Not provided"
+            console.log(data);
         })
 })
